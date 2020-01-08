2dc9c61d99c0a7d7eef63608a1c260593263c89a
LukasBrass <lukas.brasseleur@gmail.com>
2020-01-08 12:13:35 +0100

    Add clap package, add helper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Rzip")
+        .version("0.1.0")
+        .author("Hastur (Lukas Brasseleur), Iankkkk (Yani Foughali), Lexinor (Alessandro Alterno)")
+        .about("A zip & dezip tool made in Rust")
+        .arg(Arg::with_name("title")
+                 .required(true)
+                 .takes_value(true)
+                 .index(1)
+                 .help("Name of the zip file "))
+        .get_matches();
+    let zip_name = matches.value_of("title").unwrap();
+    println!("{}", zip_name);
 }
