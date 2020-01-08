01702eed5fd6e08d2b768758db54774f3c3ce7d6
lexinor <alternonolimit@gmail.com>
2020-01-08 12:39:54 +0100

    created zip / unzip command

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
         .version("0.1.0")
         .author("Hastur (Lukas Brasseleur), Iankkkk (Yani Foughali), Lexinor (Alessandro Alterno)")
         .about("A zip & dezip tool made in Rust")
-        .arg(Arg::with_name("title")
+        .arg(Arg::with_name("action")
                  .required(true)
                  .takes_value(true)
                  .index(1)
-                 .help("Name of the zip file "))
+                 .help("What do you want to do ? 'zip' / 'unzip' "))
         .get_matches();
-    let zip_name = matches.value_of("title").unwrap();
-    println!("{}", zip_name);
+    let action = matches.value_of("action").unwrap();
+    println!("{}", action);
 }
