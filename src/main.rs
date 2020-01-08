854c590e82b01c71bf950664d64c7ab58fae3ca4
LukasBrass <lukas.brasseleur@gmail.com>
2020-01-08 16:18:12 +0100

    Add two functions to zip and unzip

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 extern crate clap;
 
-use clap::{Arg, App};
+use clap::{Arg, App, SubCommand};
 
 fn main() {
     let matches = App::new("Rzip")
         .version("0.1.0")
         .author("Hastur (Lukas Brasseleur), Iankkkk (Yani Foughali), Lexinor (Alessandro Alterno)")
         .about("A zip & dezip tool made in Rust")
-        .arg(Arg::with_name("action")
-                 .required(true)
-                 .takes_value(true)
-                 .index(1)
-                 .help("What do you want to do ? 'zip' / 'unzip' "))
-        .get_matches();
+//        .subcommand(
+//            SubCommand::with_name("zip")
+                .arg(Arg::with_name("action")
+                    .required(true)
+                    .multiple(true)
+                    .number_of_values(1),
+//            ),
+    )
+    .get_matches();
     let action = matches.value_of("action").unwrap();
-    println!("{}", action);
+
+    if action == "zip" {
+        zip();
+    } else {
+        unzip();
+    }
+}
+
+fn zip() {
+    println!("Do a zip");
+}
+
+fn unzip() {
+    println!("Don't do a zip");
 }
